87d1f91cbac7a64d2fae0792e094ece90c2cfef1
PaulPreuvost <118807111+PaulPreuvost@users.noreply.github.com>
2023-03-27 16:25:55 +0200

    First Upload

---
+++ build/CMakeFiles/projet_C.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_C.dir/main.cpp.o"
+  "CMakeFiles/projet_C.dir/main.cpp.o.d"
+  "projet_C"
+  "projet_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
