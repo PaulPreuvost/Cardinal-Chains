59910e6aa1526feca00d630b5c8fa02e6e8f48be
PaulPreuvost <118807111+PaulPreuvost@users.noreply.github.com>
2023-03-27 15:51:19 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Color(int coulorInput, int coulorBack) {
+    printf("\033[%d;%dm", coulorInput, coulorBack);
+}
+
+int rand_Case() {
+    return rand() % 3;  // retourner un nombre aléatoire entre 0 et 2
+}
+
+int rand_Board_Size() {
+    return rand() % 14 + 3;  // retourner un nombre aléatoire entre 3 et 16
+}
+
+int main() {
+    srand(time(NULL));  // initialiser la graine pour rand()
+
+    int size_x, size_y;
+    do {
+        size_x = rand_Board_Size();
+        size_y = rand_Board_Size();
+    } while (size_x == size_y);
+
+    int create_Board[size_x][size_y];
+    // initialiser le tableau avec des nombres aléatoires entre 0 et 2
+    for (int i = 0; i < size_x; i++) {
+        for (int j = 0; j < size_y; j++) {
+            create_Board[i][j] = rand_Case();
+        }
+    }
+    // afficher le tableau
+    for (int i = 0; i < size_x; i++) {
+        for (int j = 0; j < size_y; j++) {
+            if (create_Board[i][j] == 0){
+                Color(33,40); // jaune sur fond noir
+            } else if (create_Board[i][j] == 1){
+                Color(31,40); // rouge sur fond noir
+            } else if (create_Board[i][j] == 2){
+                Color(34,40); // bleu sur fond noir
+            }
+            printf("%d ", create_Board[i][j]);
+            Color(0,0); // réinitialisation de la couleur
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("Rows size : %d\n", size_x);
+    printf("Columns size : %d\n", size_y);
+    return 0;
+}
